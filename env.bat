2511a4280e375c4aa3a1beb9127ff4825f4cbfb9
rafaelramos64 <rafaelmaggot64@gmail.com>
2023-08-02 09:56:58 -0300

    first commit

--- env.bat
+++ env.bat
@@ -1,3 +1,3 @@
-@set VUFIND_HOME=/home/jesielviana/dev/ibict/diadorim/diadorim-vufind
-@set VUFIND_LOCAL_DIR=/home/jesielviana/dev/ibict/diadorim/diadorim-vufind/local
+@set VUFIND_HOME=/home/rafaelramos/Projects/diadorim-vufind
+@set VUFIND_LOCAL_DIR=/home/rafaelramos/Projects/diadorim-vufind/local
 @set VUFIND_LOCAL_MODULES=Diadorim
