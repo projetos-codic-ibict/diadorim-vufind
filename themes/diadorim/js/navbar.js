11931be7ad7b22162ef90b92210eba8565fca4bf
rafaelmaggot64 <rafaelmaggot64@gmail.com>
2024-01-19 20:01:32 -0300

    fix: Corrigir bug de mudança de páginas ao clicar nos nav-links

--- themes/diadorim/js/navbar.js
+++ themes/diadorim/js/navbar.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
   watchNavbarLinks()
-  //verifyCurrentNavbarLinkActive()
 })
 
 window.onload = function () {
-  //verifyCurrentNavbarLinkActive()
+  verifyCurrentNavbarLinkActive()
 }
 
 const navbarIds = [
-  'inicialPage',
+  'initialPage',
   'navigate',
   'about',
   'register',
@@ -22,53 +21,29 @@ function watchNavbarLinks() {
     if (currentId) {
       currentId.addEventListener('click', () => {
         verify(currentId)
-        /* if (currentId.id !== id) currentId.classList.remove('active-link')
-        if (currentId.id === id) console.log('c', id)
-        if (currentId.id === id) currentId.classList.add('active-link') */
-
-        //if (window.location.pathname.includes('/diadorim/Search/Results')) currentId.classList.add('currentId')
       })
     }
   })
 }
 
 function verify(currentId) {
-  console.log('verify', currentId)
   navbarIds.forEach(id => {
     const item = document.getElementById(id)
 
-    item.classList.remove('active')
-    console.log('verify2', item)
+    item.classList.remove('active-link')
   })
 
-  currentId.classList.add('active')
+  currentId.classList.add('active-link')
 }
 
 function verifyCurrentNavbarLinkActive() {
   switch (window.location.pathname) {
     case '/diadorim/Search/Results':
       document.getElementById('navigate').classList.add('active-link')
-      console.log('c', document.getElementById('navigate'))
-      
-      navbarIds.forEach(id => {
-        let link = document.getElementById(id)
-        if (link.id !== navigate) link.classList.remove('active-link')
-      })
       break
   
     case '/diadorim/':
-      
+      document.getElementById('initialPage').classList.add('active-link')
       break
   }
-}
-
-function activateNavItem(element) {
-  // Remover a classe 'active' de todos os itens da navegação
-  var navItems = document.querySelectorAll('.nav.navbar-nav > li')
-  navItems.forEach(function(item) {
-    item.classList.remove('active')
-  })
-
-  // Adicionar a classe 'active' ao item de navegação clicado
-  element.parentNode.classList.add('active')
 }
