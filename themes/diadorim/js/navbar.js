d51d7cbf96f7807edf7f360a0cc98f122f87e629
rafaelmaggot64 <rafaelmaggot64@gmail.com>
2024-01-19 19:45:56 -0300

    teste

--- themes/diadorim/js/navbar.js
+++ themes/diadorim/js/navbar.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
   watchNavbarLinks()
-  verifyNavbarLinks(currentId)
+  //verifyCurrentNavbarLinkActive()
 })
+
+window.onload = function () {
+  //verifyCurrentNavbarLinkActive()
+}
 
 const navbarIds = [
   'inicialPage',
-  'navegate',
+  'navigate',
   'about',
   'register',
   'contact',
@@ -17,19 +21,54 @@ function watchNavbarLinks() {
     
     if (currentId) {
       currentId.addEventListener('click', () => {
-        verifyNavbarLinks(currentId)
+        verify(currentId)
+        /* if (currentId.id !== id) currentId.classList.remove('active-link')
+        if (currentId.id === id) console.log('c', id)
+        if (currentId.id === id) currentId.classList.add('active-link') */
+
+        //if (window.location.pathname.includes('/diadorim/Search/Results')) currentId.classList.add('currentId')
       })
     }
   })
 }
 
-function verifyNavbarLinks(currentLink) {
- // const navegate = document.getElementById('navegate')
-  //if (window.location.pathname.includes('/diadorim/Search/Results')) navegate.classList.add('navegate')
+function verify(currentId) {
+  console.log('verify', currentId)
+  navbarIds.forEach(id => {
+    const item = document.getElementById(id)
 
-  /* navbarIds.forEach(id => {
-    console.log('currentId', currentLink.id === id)
-    //if (currentLink.id !== id) currentLink.classList.remove('active-link')
-    if (currentLink.id === id) currentLink.classList.add('active-link')
-  }) */
+    item.classList.remove('active')
+    console.log('verify2', item)
+  })
+
+  currentId.classList.add('active')
+}
+
+function verifyCurrentNavbarLinkActive() {
+  switch (window.location.pathname) {
+    case '/diadorim/Search/Results':
+      document.getElementById('navigate').classList.add('active-link')
+      console.log('c', document.getElementById('navigate'))
+      
+      navbarIds.forEach(id => {
+        let link = document.getElementById(id)
+        if (link.id !== navigate) link.classList.remove('active-link')
+      })
+      break
+  
+    case '/diadorim/':
+      
+      break
+  }
+}
+
+function activateNavItem(element) {
+  // Remover a classe 'active' de todos os itens da navegação
+  var navItems = document.querySelectorAll('.nav.navbar-nav > li')
+  navItems.forEach(function(item) {
+    item.classList.remove('active')
+  })
+
+  // Adicionar a classe 'active' ao item de navegação clicado
+  element.parentNode.classList.add('active')
 }
