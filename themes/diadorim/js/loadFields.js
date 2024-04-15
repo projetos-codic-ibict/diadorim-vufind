a6519e1ae290c07059f3cb694b0d0fc7ad9d5917
Jesiel Viana <jesielviana@proton.me>
2024-04-15 15:16:03 -0300

    alterando contagem de selos

--- themes/diadorim/js/loadFields.js
+++ themes/diadorim/js/loadFields.js
@@ -68,7 +68,7 @@ async function getQttSealsByColor() {
 
     for (const seal of sealColor) {
       for (const info of sealsInfo) {
-        if (seal.value.startsWith(info.color) && seal.count > info.quantity) {
+        if (seal.value.startsWith(info.color)) {
           info.quantity = seal.count
           info.href = seal.href
         }
