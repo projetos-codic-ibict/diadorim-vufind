1616539164732cdfad3726acfc19edc461a994db
Jesiel Viana <jesielviana@proton.me>
2024-05-29 18:55:34 -0300

    add módulo de exportação

--- module/Diadorim/config/module.config.php
+++ module/Diadorim/config/module.config.php
@@ -1,49 +1,91 @@
 <?php
 
-return array (
-  'vufind' => 
-  array (
-    'plugin_managers' => 
-    array (
-      'recorddriver' => 
-      array (
-        'factories' => 
-        array (
+return array(
+  'vufind' =>
+  array(
+    'plugin_managers' =>
+    array(
+      'recorddriver' =>
+      array(
+        'factories' =>
+        array(
           'Diadorim\\RecordDriver\\SolrDefault' => 'VuFind\\RecordDriver\\SolrDefaultFactory',
         ),
-        'aliases' => 
-        array (
+        'aliases' =>
+        array(
           'VuFind\\RecordDriver\\SolrDefault' => 'Diadorim\\RecordDriver\\SolrDefault',
         ),
       ),
     ),
   ),
-  'controllers' => 
-  array (
-    'factories' => 
-    array (
+  'controllers' =>
+  array(
+    'factories' =>
+    array(
       'Diadorim\\Controller\\SobreController' => 'VuFind\\Controller\\AbstractBaseFactory',
+      'Diadorim\\Controller\\BulkExportController' => 'VuFind\\Controller\\AbstractBaseFactory',
     ),
-    'aliases' => 
-    array (
+    'aliases' =>
+    array(
       'Sobre' => 'Diadorim\\Controller\\SobreController',
       'sobre' => 'Diadorim\\Controller\\SobreController',
+      'BulkExport' => 'Diadorim\\Controller\\BulkExportController',
+      'bulkexport' => 'Diadorim\\Controller\\BulkExportController',
     ),
   ),
-  'router' => 
-  array (
-    'routes' => 
-    array (
-      'sobre-home' => 
-      array (
+  'router' =>
+  array(
+    'routes' =>
+    array(
+      'sobre-home' =>
+      array(
         'type' => 'Laminas\\Router\\Http\\Literal',
-        'options' => 
-        array (
+        'options' =>
+        array(
           'route' => '/Sobre/Home',
-          'defaults' => 
-          array (
+          'defaults' =>
+          array(
             'controller' => 'Sobre',
             'action' => 'Home',
+          ),
+        ),
+      ),
+      'bulkexport-home' =>
+      array(
+        'type' => 'Laminas\\Router\\Http\\Literal',
+        'options' =>
+        array(
+          'route' => '/bulkexport/home',
+          'defaults' =>
+          array(
+            'controller' => 'BulkExport',
+            'action' => 'Home',
+          ),
+        ),
+      ),
+      'bulkexport-csv' =>
+      array(
+        'type' => 'Laminas\\Router\\Http\\Literal',
+        'options' =>
+        array(
+          'route' => '/bulkexport/csv',
+          'defaults' =>
+          array(
+            'controller' => 'BulkExport',
+            'action' => 'CSV',
+          ),
+        ),
+      ),
+      'bulkexport-download' =>
+      array(
+        'type' => 'Laminas\\Router\\Http\\Literal',
+        'options' =>
+        array(
+          'route' => '/bulkexport/download',
+          'defaults' =>
+          array(
+            'controller' => 'BulkExport',
+            'action' => 'Download',
           ),
         ),
       ),
